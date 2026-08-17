e5c56133a6afeb4e91b0aecb42a30e305c85b2e5
devanshpatel13579-cloud <devanshpatel13579@gmail.com>
2026-08-17 21:14:07 +0530

    day 2

---
+++ rect.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    float length, width, area, perimeter;
+
+    printf("Enter length of rectangle: ");
+    scanf("%f", &length);
+
+    printf("Enter width of rectangle: ");
+    scanf("%f", &width);
+
+    area = length * width;
+    perimeter = 2 * (length + width);
+
+    printf("Area = %.2f\n", area);
+    printf("Perimeter = %.2f", perimeter);
+    return 0;
+}
